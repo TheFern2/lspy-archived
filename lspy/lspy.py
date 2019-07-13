062f2dcbccad142386411dc8304b74790646dc04
kodaman2 <ic3balandran@yahoo.com>
2019-07-13 03:00:57 -0500

    Starting utility develop mode

---
+++ lspy/lspy.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+  
+def main():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-l', '--list', dest='list_files', help='list files', action='store_true')
+        parser.set_defaults(list_files=False)
+        args = parser.parse_args()
+  
+        dir_list = os.listdir()
+        if args.list_files:
+            print(" ".join(dir_list))
+ 
+ 
+if __name__ == '__main__':
+          main()
